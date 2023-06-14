dc7070593b61314ba9a7344044d17ff05b5afad5
Denzel <jdenzelmab@gmail.com>
2023-06-14 18:25:25 -0400

    Completed react hooks forms lab

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+
+  const [addItem, setAdd] = useState("")
+  const [itemCategory, setCategory] = useState("Produce")
+
+  function handleAdd(event)  {
+    setAdd(event.target.value)
+  }
+
+  function handleCategory(event) {
+    setCategory(event.target.value)
+  }
+
+  function handleSubmit(event)  {
+    event.preventDefault();
+    const newItem = {
+      id: uuid(), 
+      name: addItem,
+      category: itemCategory,
+    };
+  onItemFormSubmit(newItem)
+  setAdd("")
+  setCategory("Produce")
+  
+  }
+
+  
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit} >
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={addItem} onChange={handleAdd}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={itemCategory} onChange={handleCategory}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
